6969d2c5e1c97d0c5610f846886d8a4a92eca4f8
iambnlvn <iambnlvn@gmail.com>
2024-11-01 13:36:55 -0500

    feat: Add MSR check and A20 enable code
    This commit adds code to check for the presence of the MSR (Model Specific Register) and enable the A20 line. The MSR check ensures that the necessary CPU feature is available, and the A20 enable code enables the A20 line, which is required for proper memory addressing.

--- src/boot/bios/stage2.s
+++ src/boot/bios/stage2.s
@@ -39,10 +39,74 @@ checkCpuId:
     jmp error
     .hasCpuId:
 
+checkMsr:
+    mov dword [24], .noMsr
+    mov ecx, 0xC0000080
+    rdmsr
+    jmp .hasMsr
+    .no_msr:
+    mov si, noMsrError
+    jmp error
+    .hasMsr:
+
+checkA20:
+    mov	ax,0
+	mov	es,ax
+	mov	ax,0xFFFF
+	mov	fs,ax
+	mov	byte [es:0x600],0
+	mov	byte [fs:0x610],0xFF
+	cmp	byte [es:0x600],0xFF
+	je	.enabled
+	stc
+	ret
+	.enabled: 
+	clc
+	ret
+
+enableA20:
+    cli
+    call check_a20
+    jc .a20_enabled
+    mov ax, 0x2401
+    int 0x15
+    call check_a20
+    jc .a20_enabled
+    mov si, errorEnableA20
+    jmp error
+    .a20Enabled:
+    sti
+    jmp identityPaging
+
+identityPaging:
+    mov eax, pageDirectory / 16
+    mov es, ax
+
+    xor eax, eax
+    mov ecx, 0x400
+    xor di, di
+    rep stosd
+
+    mov dword [es: 0x3ff * 4], pageDirectory | 3
+    mov dword [es: 0], (pageDirectory + 0x1000) | 3
+
+    mov edi, 0x1000
+    mov cx, 0x400
+    mov eax, 3
+    .loop:
+    mov [es:edi], eax
+    add edi, 4
+    add eax, 0x1000
+    loop .loop
+
+    mov eax, pageDirectory
+    mov cr3, eax
 
 driveNumber db 0
-partition_entry dw 0
+partitionEntry dw 0
 sectorCount dw 0
 headCount dw 0
 pciError: db "Error: could not find PCI bus", 0
 noCpuIdError: db "Error: could not find CPUID", 0
+noMsrError: db "Error: could not find MSR", 0
+errorEnableA20: db "Error: could not enable A20 line", 0
