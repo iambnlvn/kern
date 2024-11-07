02bb65643f08e0eed7b1f9e825a9b920e4489db5
iambnlvn <iambnlvn@gmail.com>
2024-11-06 19:34:13 -0600

    feat: implement kernel buffer allocation and loading mmap

--- src/boot/bios/stage2.s
+++ src/boot/bios/stage2.s
@@ -7,7 +7,7 @@
 %define kernelFileEntry 0x8800      
 %define pageDirectory 0x40000        
 %define pageDirectoryLength 0x20000   
-%define memoryMap 0x60000            
+%define mMap 0x60000            
 %define temporaryLoadBuffer 0x9000    
 %define partitionOffset (0x800 * 0x200)
 %define kernelSector 0x10
@@ -67,11 +67,11 @@ checkA20:
 enableA20:
     cli
     call check_a20
-    jc .a20_enabled
+    jc .a20Enabled
     mov ax, 0x2401
     int 0x15
-    call check_a20
-    jc .a20_enabled
+    call checkA20
+    jc .a20Enabled
     mov si, errorEnableA20
     jmp error
     .a20Enabled:
@@ -102,11 +102,223 @@ identityPaging:
     mov eax, pageDirectory
     mov cr3, eax
 
+loadGDT:
+    lgdt [lgdtData.gdt]
+
+informBiosMixMode:
+    mov eax, 0xec00
+    mov ebx, 3
+    int 0x15
+
+loadMmap:
+    xor ebx, ebx
+    xor ax, ax
+    mov es, ax
+    mov ax, mmap / 16
+    mov fs, ax
+
+    .loop:
+    mov di, .entry
+    mov edx, 0x534D4150
+    mov ecx, 24
+    mov eax, 0xe820
+    mov byte [.acpi], 1
+    int 0x15
+    jc .finished
+
+    cmp eax, 0x534D4150
+    jne .fail
+
+    cmp dword [.type], 1
+    jne .tryNext
+    cmp dword [.size], 0
+    je .tryNext
+    cmp dword [.acpi], 0
+    je .tryNext
+
+    mov eax, [.size]
+    and eax, ~0x3ff
+    or eax, eax
+    jz .tryNext
+
+    .baseGood:
+    mov eax, [.base]
+    and eax, 0xfff
+    or eax, eax
+    jz .base_aligned
+    mov eax, [.base]
+    and eax, ~0xfff
+    add eax, 0x1000
+    mov [.base], eax
+    sub dword [.size], 0x1000
+    sbb dword [.size + 4], 0
+
+    .baseAligned:
+    mov eax, [.size]
+    and eax, ~0xfff
+    mov [.size], eax
+
+    mov eax, [.size]
+    shr eax, 12
+    push ebx
+    mov ebx, [.size + 4]
+    shl ebx, 20
+    add eax, ebx
+    pop ebx
+    mov [.size], eax
+    mov dword [.size + 4], 0
+
+    push ebx
+    mov ebx, [.pointer]
+    mov eax, [.base]
+    mov [fs:bx], eax
+    mov eax, [.base + 4]
+    mov [fs:bx + 4], eax
+    mov eax, [.size]
+    mov [fs:bx + 8], eax
+    add [.totalMem], eax
+    mov eax, [.size + 4]
+    adc [.totalMem + 4], eax
+    mov [fs:bx + 12], eax
+    add dword [.pointer], 16
+    pop ebx
+
+    .tryNext:
+        or ebx, ebx
+        jnz .loop
+    
+    .finished:
+      mov eax, [.pointer]
+    shr eax, 4
+    or eax, eax
+    jz .fail
+
+    mov ebx, [.pointer]
+    mov dword [fs:bx], 0
+    mov dword [fs:bx + 4], 0
+    mov eax, [.totalMem]
+    mov dword [fs:bx + 8], eax
+    mov eax, [.totalMem + 4]
+    mov dword [fs:bx + 12], eax
+    jmp allocKernBuff
+
+    .fail:
+    mov si, memoryMapGettingError
+    jmp error
+
+    .pointer:
+        dd 0
+    .entry:
+        .base: dq 0
+        .size: dq 0
+        .type: dd 0
+        .acpi: dd 0
+    .totalMem: dq 0
+
+allocKernBuff:
+    push ds
+    push es
+    push ss
+    cli
+    mov eax, cr0
+    or eax, 0x80000001
+    mov cr0, eax
+    jmp 0x8:.protectedMode
+
+    [bits 32]
+    .protectedMode:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+
+    mov ecx, [kernelSize]
+    shr ecx, 12
+    inc ecx
+    mov edx, ecx
+    shl edx, 12
+
+    xor ebx, ebx
+
+    .memRegionLoop:
+    mov eax, [ebx + mMap + 4]
+    or eax, eax
+    jnz .tryNextMemRegion
+    mov eax, [ebx + mMap]
+    cmp eax, 0x100000
+    jne .tryNextMemRegion
+
+    mov eax, [ebx + mMap + 8]
+    cmp eax, ecx
+    jl .tryNextMemRegion
+
+    mov eax, [ebx + mMap + 0]
+    mov [kernBuffer], eax
+    add eax, edx
+    mov [ebx + mMap + 0], eax
+    sub dword [ebx + mMap + 8], ecx
+    sbb dword [ebx + mMap + 12], 0
+
+    jmp .foundBuffer
+
+    .tryNextMemRegion:
+    add ebx, 16
+    mov eax, [loadMmap.pointer]
+    cmp ebx, eax
+    jne .memRegionLoop
+    mov si, noMemory
+    jmp error32
+
+    .foundBuffer:
+    mov eax, cr0
+    and eax, 0x7FFFFFFF
+    mov cr0, eax
+    jmp 0x18:.realMode
+
+    [bits 16]
+    .realMode:
+    mov eax, cr0
+    and eax, 0x7FFFFFFE
+    mov cr0, eax
+    jmp 0x0:.finish
+
+    .finish:
+    pop ss
+    pop es
+    pop ds
+
+
+error32:
+    mov eax, cr0
+    and eax, 0x7FFFFFFF
+    mov cr0, eax
+    jmp 0x18:.realMode
+
+    [bits 16]
+    .real_mode:
+    mov realMode, cr0
+    and eax, 0x7FFFFFFE
+    mov cr0, eax
+    jmp 0x0:.finish
+
+    .finish:
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    jmp error
+
 driveNumber db 0
 partitionEntry dw 0
 sectorCount dw 0
 headCount dw 0
+
+kernBuffer dq 0
+kernelSize dd 0
+
 pciError: db "Error: could not find PCI bus", 0
 noCpuIdError: db "Error: could not find CPUID", 0
 noMsrError: db "Error: could not find MSR", 0
 errorEnableA20: db "Error: could not enable A20 line", 0
+memoryMapGettingError: db "Error: could not get memory map", 0
+noMemory: db "Error: could not find memory for kernel", 0
