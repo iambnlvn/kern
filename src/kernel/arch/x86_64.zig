d494b785a90e039caf64efcd5c88e859c591094a
iambnlvn <iambnlvn@gmail.com>
2024-12-02 23:04:13 -0600

    feat: Implement InterruptHandler and handlePageFault

--- src/kernel/arch/x86_64.zig
+++ src/kernel/arch/x86_64.zig
@@ -3,6 +3,19 @@ const kernel = @import("./../kernel.zig");
 const Thread = kernel.scheduling.Thread;
 const List = kernel.ds.List;
 const Mutex = kernel.sync.Mutex;
+const zeroes = kernel.zeroes;
+const memory = kernel.memory;
+pub const kernelAddrSpaceStart = 0xFFFF900000000000;
+pub const kernelAddrSpaceSize = 0xFFFFF00000000000 - 0xFFFF900000000000;
+pub const pageSize = 0x1000;
+pub const lowMemMapStart = 0xFFFFFE0000000000;
+pub const lowMemLimit = 0x100000000;
+pub const coreMemStartRegion = 0xFFFF8001F0000000;
+pub const coreMemRegionCount = (0xFFFF800200000000 - 0xFFFF8001F0000000) / @sizeOf(memory.Region);
+pub const coreAddrSpaceStart = 0xFFFF800100000000;
+pub const coreAddrSpaceSize = 0xFFFF8001F0000000 - 0xFFFF800100000000;
+pub const modulesStart = 0xFFFFFFFF90000000;
+pub const modulesSize = 0xFFFFFFFFC0000000 - 0xFFFFFFFF90000000;
 
 pub fn initThread(
     kernelStack: u64,
@@ -100,6 +113,8 @@ pub extern fn halt() callconv(.C) noreturn;
 pub extern fn enableInterrupts() callconv(.C) void;
 pub extern fn debugOutByte(byte: u8) callconv(.C) void;
 pub extern fn disableInterrupts() callconv(.C) void;
+pub extern fn fakeTimerInterrupt() callconv(.C) void;
+extern fn MMArchSafeCopy(dest: u64, source: u64, byteCount: u64) callconv(.C) bool;
 
 const LocalStorage = extern struct {
     currentThread: ?*Thread,
@@ -166,16 +181,63 @@ export fn InterruptHandler(ctx: *InterruptContext) callconv(.C) void {
             if (ctx.cs != 0x5b and ctx.cs != 0x6b) std.debug.panic("Invalid CS", .{});
             const currentThread = getCurrentThread().?;
             if (currentThread.isKernelThread) std.debug.panic("Kernel thread is executing user code", .{});
-
+            const prevTerminatableState = currentThread.terminatableState.readVolatile();
             currentThread.terminatableState.writeVolatile(.inSysCall);
 
             if (maybeLS) |ls| if (ls.spinlockCount != 0) std.debug.panic("user exception occurred with spinlock acquired", .{});
 
             enableInterrupts();
             maybeLS = null;
+            if ((interrupt == 14 and !handlePageFault(ctx.cr2, if (ctx.errorCode & 2 != 0) memory.HandlePageFaultFlags.fromFlag(.write) else memory.handlePageFaultFlags.empty())) or interrupt != 14) {
+                const rbp = ctx.rbp;
+                const traceDepth: u32 = 0;
+                while (rbp != 0 and traceDepth < 32) {
+                    if (!MMArchIsBufferInUserRange(rbp, 16)) break;
+                    var val: u64 = 0;
+                    if (!MMArchSafeCopy(@intFromPtr(&val), rbp + 8, @sizeOf(u64))) break;
+                    if (val == 0) break;
+                    if (!MMArchSafeCopy(@intFromPtr(&rbp), rbp, @sizeOf(u64))) break;
+                }
 
-            // TODO!: I have no clue what to do next
+                var crashReason = zeroes(kernel.CrashReason);
+                crashReason.errorCode = kernel.FatalError.processorException;
+                crashReason.duringSysCall = -1;
+                currentThread.process.?.crash(&crashReason);
+            }
+
+            if (currentThread.terminatableState.readVolatile() != .inSysCall) std.debug.panic("Thread changed terminatable state during interrupt", .{});
+
+            currentThread.terminatableState.writeVolatile(prevTerminatableState);
+            if (currentThread.terminating.readVolatile() or currentThread.paused.readVolatile()) fakeTimerInterrupt();
+            disableInterrupts();
+        } else {
+            if (ctx.cs != 0x48) std.debug.panic("Invalid CS", .{});
+            if (interrupt == 14) {
+                if (ctx.errorCode & (1 << 3) != 0) std.debug.panic("unresolvable page fault", .{});
+
+                if (maybeLS) |local| if (local.spinlockCount != 0 and (ctx.cr2 >= 0xFFFF900000000000 and ctx.cr2 < 0xFFFFF00000000000)) std.debug.panic("page fault occurred with spinlocks active", .{});
+
+                if (ctx.fxsave & 0x200 != 0 and ctx.cr8 != 0xe) {
+                    enableInterrupts();
+                    maybeLS = null;
+                }
+
+                const res = handlePageFault(ctx.cr2, (if (ctx.errorCode & 2 != 0) memory.HandlePageFaultFlags.fromFlag(.write) else memory.HandlePageFaultFlags.empty()).orFlag(.forSupervisor));
+                var safeCopy = false;
+                if (!res) {
+                    if (getCurrentThread().?.inSafeCopy and ctx.cr2 < 0x8000000000000000) {
+                        safeCopy = true;
+                        ctx.rip = ctx.r8;
+                    }
+                }
+
+                if (res or safeCopy) disableInterrupts() else std.debug.panic("Unhandleable page fault", .{});
+            } else {
+                std.debug.panic("Unable to resolve exception", .{});
+            }
         }
+    } else if ((interrupt == 0xFF) or (interrupt >= 0x20 and interrupt < 0x30) or (interrupt >= 0xF0 and interrupt < 0xFE)) {
+        std.debug.panic("Not implemented", .{});
     }
 }
 
@@ -239,4 +301,51 @@ fn interruptHandlerMaker(comptime num: u64, comptime hasErrorCode: bool) type {
             unreachable;
         }
     };
+}
+//This is currently not fully implemented, acts as a placeHolder
+pub export fn handlePageFault(faultyAddr: u64, flags: memory.HandlePageFaultFlags) bool {
+    const va = faultyAddr & ~@as(u64, pageSize - 1);
+    const forSupervisor = flags.contains(.forSupervisor);
+
+    if (!areInterruptsEnabled()) {
+        std.debug.panic("Page fault with interrupts disabled\n", .{});
+    }
+
+    const faultInVeryLowMem = va < pageSize;
+    if (!faultInVeryLowMem) {
+        if (va >= lowMemMapStart and va < lowMemMapStart + lowMemLimit and forSupervisor) {
+            const physicalAddr = va - lowMemMapStart;
+            const mapPageFlags = memory.MapPageFlags.fromFlag(.commitTablesNow);
+            _ = mapPage(&kernel.addrSpace, physicalAddr, va, mapPageFlags); //TODO!: implement mapPage
+            return true;
+        } else if (va >= coreMemStartRegion and va < coreMemStartRegion + coreMemRegionCount * @sizeOf(memory.Region) and forSupervisor) {
+            const physicalAllocationFlags = memory.Physical.Flags.fromFlag(.zeroed);
+            const physicalAddr = memory.physicalAllocWithFlags(physicalAllocationFlags);
+            const mapPageFlags = memory.MapPageFlags.fromFlag(.commitTablesNow);
+            _ = mapPage(&kernel.addrSpace, physicalAddr, va, mapPageFlags);
+            return true;
+        } else if (va >= coreAddrSpaceStart and va < coreAddrSpaceStart + coreAddrSpaceSize and forSupervisor) {
+            return kernel.coreAddrSpace.handlePageFault(va, flags);
+        } else if (va >= kernelAddrSpaceStart and va < kernelAddrSpaceSize and forSupervisor) {
+            return kernel.addrSpace.handlePgaeFault(va, flags);
+        } else if (va >= modulesStart and va < modulesStart + modulesSize and forSupervisor) {
+            return kernel.addrSpace.handlePageFault(va, flags);
+        } else {
+            if (getCurrentThread()) |currentThread| {
+                const space = if (currentThread.TempAddrSpace) |tempAddrSpace| @as(*memory.AddressSpace, @ptrCast(tempAddrSpace)) else currentThread.process.?.addrSpace;
+                return space.handlePageFault(va, flags);
+            } else {
+                std.debug.panic("unreachable path\n,", .{});
+            }
+        }
+    }
+
+    return false;
+}
+
+export fn MMArchIsBufferInUserRange(baseAddr: u64, byteCount: u64) callconv(.C) bool {
+    if (baseAddr & 0xFFFF800000000000 != 0) return false;
+    if (byteCount & 0xFFFF800000000000 != 0) return false;
+    if ((baseAddr + byteCount) & 0xFFFF800000000000 != 0) return false;
+    return true;
 }
