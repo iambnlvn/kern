d8bb6ff437440a0660a4bb52877f3b2252d4932d
iambnlvn <iambnlvn@gmail.com>
2024-12-03 19:47:56 -0600

    feat: Expand memory management structures with AddressSpace and PageFrame implementations

--- src/kernel/memory.zig
+++ src/kernel/memory.zig
@@ -1,10 +1,20 @@
+const std = @import("std");
 const kernel = @import("kernel.zig");
 const Volatile = kernel.Volatile;
-const Mutex = kernel.sync.Mutex;
+const sync = kernel.sync;
+const Mutex = sync.Mutex;
+const SpinLock = sync.SpinLock;
+const Event = sync.Event;
 const WriterLock = kernel.sync.WriterLock;
 const AVLTree = kernel.ds.AVLTree;
 const LinkedList = kernel.ds.LinkedList;
 const Bitflag = kernel.ds.Bitflag;
+const List = kernel.ds.List;
+const arch = @import("./arch/x86_64.zig");
+const scheduling = @import("scheduling.zig");
+const Process = scheduling.Process;
+const Thread = scheduling.Thread;
+const pageSize = arch.pageSize;
 
 pub const SharedRegion = extern struct {
     size: u64,
@@ -85,3 +95,153 @@ pub const HandlePageFaultFlags = Bitflag(enum(u32) {
     lockAcquired = 1,
     forSupervisor = 2,
 });
+
+pub const AddressSpace = extern struct {
+    arch: AddressSpace.Arch,
+    freeRegionBase: AVLTree(Region),
+    freeRegionSize: AVLTree(Region),
+    freeRegionsNonGuard: LinkedList(Region),
+    usedRegions: AVLTree(Region),
+    reserveMutex: Mutex,
+    refCount: Volatile(i32),
+    isUser: bool,
+    commitCount: u64,
+    reserveCount: u64,
+    // removeAsyncTask: AsyncTask, //TODO!: implement AsyncTask
+
+    const Self = @This();
+
+    pub fn findRegion(self: *Self, addr: u64) ?*Region {
+        self.reserveMutex.assertLocked();
+
+        if (self == &kernel.coreAddressSpace) {
+            for (kernel.mmCoreRegions[0..kernel.mmCoreRegionCount]) |*region| {
+                if (region.u.core.used and region.descriptor.baseAddr <= addr and region.descriptor.baseAddr + region.descriptor.pageCount * pageSize > addr) {
+                    return region;
+                } else {
+                    return null;
+                }
+            }
+        } else {
+            const node = self.usedRegions.find(addr, .LargestBelowOrEqual) orelse return null;
+
+            const region = node.value.?;
+            if (region.descriptor.baseAddr > addr) std.debug.panic("Unvalid regions tree", .{});
+            if (region.descriptor.baseAddr + region.descriptor.pageCount * pageSize <= addr) return null;
+            return region;
+        }
+    }
+};
+
+pub const MapPageFlags = Bitflag(enum(u32) {
+    notCacheable = 0,
+    user = 1,
+    overwrite = 2,
+    commitTablesNow = 3,
+    readOnly = 4,
+    copied = 5,
+    noNewTables = 6,
+    frameLockAquired = 7,
+    writeCombining = 8,
+    ignoreIfMapped = 9,
+});
+
+pub const PageFrame = extern struct {
+    state: Volatile(PageFrame.State),
+    flags: Volatile(u8),
+    cacheRef: ?*volatile u8,
+    u: extern union {
+        list: extern union {
+            next: Volatile(u64),
+            prev: ?*volatile u64,
+        },
+        active: extern struct {
+            refs: Volatile(u64),
+        },
+    },
+    pub const State = enum(i8) {
+        unusable,
+        bad,
+        zeroed,
+        free,
+        standby,
+        active,
+        modified,
+        modifiedNoWrite,
+        modifiedNoWriteNoRead,
+        modifiedNoRead,
+        modifiedNoReadNoWrite,
+        modifiedNoReadNoWriteNoExecute,
+        modifiedNoReadNoExecute,
+        modifiedNoWriteNoExecute,
+    };
+};
+
+const ObjectCache = extern struct {
+    lock: SpinLock,
+    items: List,
+    count: u64,
+    trim: fn (cache: *ObjectCache) callconv(.C) bool,
+    trimLock: WriterLock,
+    node: LinkedList(ObjectCache).Node,
+    averageObjBytes: u64,
+
+    const trimGroupeCount = 1024;
+};
+pub const Physical = extern struct {
+    pub const Allocator = extern struct {
+        pageFrames: [*]PageFrame,
+        pageFrameDBInitialized: bool,
+        pageFrameDBCount: u64,
+
+        firstFreePage: u64,
+        firstZeroedPage: u64,
+        firstStandbyPage: u64,
+        firstModifiedPage: u64,
+        firstModifiedNoWritePage: u64,
+        firstModifiedNoWriteNoReadPage: u64,
+        firstModifiedNoReadPage: u64,
+        firstModifiedNoReadNoWritePage: u64,
+        firstModifiedNoReadNoWriteNoExecutePage: u64,
+        firstModifiedNoReadNoExecutePage: u64,
+        firstModifiedNoWriteNoExecutePage: u64,
+        // freeOrZeroedPageBitset: BitSet, //TODO!: implement BitSet
+        zeroedPageCount: u64,
+        standbyPageCount: u64,
+        modifiedPageCount: u64,
+        modifiedNoWritePageCount: u64,
+        modifiedNoWriteNoReadPageCount: u64,
+        modifiedNoReadPageCount: u64,
+        modifiedNoReadNoWritePageCount: u64,
+        modifiedNoReadNoWriteNoExecutePageCount: u64,
+        modifiedNoReadNoExecutePageCount: u64,
+        modifiedNoWriteNoExecutePageCount: u64,
+        commitedFixed: i64,
+        commitPageable: i64,
+        commitFixedLimit: i64,
+        commitLimit: i64,
+        commitMutex: Mutex,
+        pageFrameMutex: Mutex,
+
+        manipulationLock: Mutex,
+        manipulationProcLock: SpinLock,
+        manipulationRegion: ?*Region,
+        zeroPageThread: *Thread,
+        zeroPageEvent: Event,
+
+        objCacheList: LinkedList(ObjectCache),
+        objCacheListMutex: Mutex,
+
+        availableCriticalEvent: Event,
+        availableLowEvent: Event,
+        availableNormalEvent: Event,
+        availableHighEvent: Event,
+
+        approxTotalObjCacheByteCount: u64,
+        trimObjCacheEvent: Event,
+        nextProcToBalance: ?*Process,
+        nextRegionToBalance: ?*Region,
+        balanceResumePosition: u64,
+        //TODO!: implement allocator methods
+    };
+};
