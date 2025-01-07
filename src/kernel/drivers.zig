f967e91a4ff56995c8eff26b985edbc7b4780a74
iambnlvn <iambnlvn@gmail.com>
2025-01-07 07:50:33 -0600

    feat: add SVGA driver initialization and screen update functions

--- src/kernel/drivers.zig
+++ src/kernel/drivers.zig
@@ -1,2 +1,3 @@
 pub const serial = @import("./drivers/serial.zig");
 pub const ACPI = @import("./drivers/ACPI.zig");
+pub const SVGA = @import("./drivers/SVGA.zig");
--- src/kernel/drivers/SVGA.zig
+++ src/kernel/drivers/SVGA.zig
@@ -13,6 +13,10 @@ pub const Driver = struct {
     pixelCountX: u32,
     pixelCountY: u32,
 
+    pub fn init() void {
+        driver.setup();
+    }
+
     fn setup(self: *@This()) void {
         info = @as(?*VideoModeInformation, @ptrFromInt(kernel.addrSpace.mapPhysical(0x7000 + kernel.arch.bootloaderInfoOffset, @sizeOf(VideoModeInformation), kernel.memory.Region.Flags.empty())));
 
@@ -36,6 +40,25 @@ pub const Driver = struct {
 
         if (info.bitsPerPixel != 32) {
             kernel.panicf("Only 32 bit pixels are supported. VBE pixel count: {}", .{info.bitsPerPixel});
+        }
+    }
+
+    pub fn updateScreen(self: *@This(), sourcePtr: [*]const u8, sourceW: u32, sourceH: u32, sourceRowStride: u32, destX: u32, destY: u32) void {
+        if (destX >= self.width or sourceW > self.width - destX or destY >= self.height or sourceH > self.height - destY) {
+            kernel.panic("Update region outside graphics target bounds");
+        }
+
+        var destRowStart = @as([*]u32, @ptrFromInt(@intFromPtr(self.linearFramebuffer) + destX * @sizeOf(u32) + destY * self.pixelCountY));
+        const sourceRowStart = @as([*]const u32, @ptrCast(sourcePtr));
+
+        var y: u64 = 0;
+        while (y < sourceH) : (y += 1) {
+            const destination = destRowStart[0..sourceW];
+            const source = sourceRowStart[0..sourceW];
+            @memcpy(destination, source);
+
+            destRowStart += self.pixelCountY / 4;
+            sourceRowStart += sourceRowStride / 4;
         }
     }
 };
