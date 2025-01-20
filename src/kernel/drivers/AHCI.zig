f1ad3711cf1f7dcbfc8095b1892e7c691e576d39
iambnlvn <iambnlvn@gmail.com>
2025-01-20 15:54:35 -0600

    feat: add MBR partition detection and filesystem initialization in AHCI driver

--- src/kernel/drivers/AHCI.zig
+++ src/kernel/drivers/AHCI.zig
@@ -3,6 +3,7 @@ const kernel = @import("../kernel.zig");
 const PCI = kernel.drivers.PCI;
 const Mutex = kernel.sync.Mutex;
 const arch = @import("../arch/x86_64.zig");
+const kUtils = @import("../kernelUtils.zig");
 
 const DMASegment = extern struct {
     physicalAddr: u64,
@@ -52,7 +53,41 @@ const MBR = extern struct {
         count: u32,
         present: bool,
     };
+    fn getPartitions(firstBlk: [*]u8, sectorCount: u64) bool {
+        const isMagicBootOk = firstBlk[510] == 0x55 and firstBlk[511] == 0xaa;
+        if (!isMagicBootOk) return false;
+
+        const partitionEntrySize = 16;
+        const partitionTableOffset = 0x1be;
+
+        for (driver.mbrPartitions, 0..) |*mbrPartition, i| {
+            const entryOffset = partitionTableOffset + i * partitionEntrySize;
+
+            if (firstBlk[entryOffset + 4] == 0) {
+                mbrPartition.present = false;
+                continue;
+            }
+
+            mbrPartition.offset = (@as(u32, @intCast(firstBlk[entryOffset + 8])) << 0) +
+                (@as(u32, @intCast(firstBlk[entryOffset + 9])) << 8) +
+                (@as(u32, @intCast(firstBlk[entryOffset + 10])) << 16) +
+                (@as(u32, @intCast(firstBlk[entryOffset + 11])) << 24);
+            mbrPartition.count = (@as(u32, @intCast(firstBlk[entryOffset + 12])) << 0) +
+                (@as(u32, @intCast(firstBlk[entryOffset + 13])) << 8) +
+                (@as(u32, @intCast(firstBlk[entryOffset + 14])) << 16) +
+                (@as(u32, @intCast(firstBlk[entryOffset + 15])) << 24);
+            mbrPartition.present = true;
+
+            if (mbrPartition.offset > sectorCount or mbrPartition.count > sectorCount - mbrPartition.offset or mbrPartition.count < 32) {
+                return false;
+            }
+            driver.mbrPartitionsCount += 1;
+        }
+
+        return true;
+    }
 };
+pub var driver: *Driver = undefined;
 
 pub const Driver = struct {
     drives: []Drive,
@@ -86,11 +121,27 @@ pub const Driver = struct {
     };
 
     const MAX_PORT_COUNT = 32;
+
+    pub fn init() void {
+        const driveOffset = kUtils.roundUp(u64, @sizeOf(Driver), @alignOf(Drive));
+        const partitionDevicesOffset = kUtils.roundUp(u64, driveOffset + (@sizeOf(Drive) * Drive.maxCount), @alignOf(PartitionDevice));
+        const allocSize = partitionDevicesOffset + (@sizeOf(PartitionDevice) * PartitionDevice.maxCount);
+        const address = kernel.addrSpace.alloc(allocSize, kernel.memory.Region.Flags.fromFlag(.fixed), arch.modulePtr, true);
+        if (address == 0) kernel.panic("Could not allocate memory for PCI driver");
+        arch.modulePtr += kUtils.roundUp(u64, allocSize, arch.pageSize);
+        driver = @as(*Driver, @ptrFromInt(address));
+        driver.drives.ptr = @as([*]Drive, @ptrFromInt(address + driveOffset));
+        driver.drives.len = 0;
+        driver.partitionDevices.ptr = @as([*]PartitionDevice, @ptrFromInt(address + partitionDevicesOffset));
+        driver.partitionDevices.len = 0;
+        //TODO!: implement a setup
+    }
 };
 
 // TODO!: Implement this
 const PartitionDevice = extern struct {
     sectorOffset: u64,
+    const maxCount = 64;
 };
 
 pub const Drive = extern struct {
@@ -108,6 +159,7 @@ const DriveType = enum(u8) {
     usbStorage = 4,
 };
 
+const SIGNATURE_BLOCK_SIZE = 65536;
 const BlockDevice = extern struct {
     access: u64,
     sectorSize: u64,
@@ -123,4 +175,57 @@ const BlockDevice = extern struct {
 
     const read = 0;
     const write = 1;
+
+    const AccessRequest = extern struct {
+        device: *BlockDevice,
+        offset: u64,
+        count: u64,
+        operation: i32,
+        buffer: *DMABuffer,
+        flags: Flags,
+        dispatch_group: ?*kernel.Workgroup,
+
+        const Flags = kernel.ds.Bitflag(enum(u64) {
+            cache = 0,
+            softErrors = 1,
+        });
+        const Callback = fn (self: @This()) i64;
+    };
+    fn detectFilesystem(self: *@This()) void {
+        _ = self.detectFilesystemMutex.acquire();
+        defer self.detectFilesystemMutex.release();
+
+        if (self.nestLevel > 4) kernel.panic("Filesystem nest limit");
+
+        const sectorsToRead = (SIGNATURE_BLOCK_SIZE + self.sectorSize - 1) / self.sectorSize;
+        if (sectorsToRead > self.sectorCount) kernel.panic("Drive too small");
+
+        const bytesToRead = sectorsToRead * self.sectorSize;
+        self.signatureBlock = @as(?[*]u8, @ptrFromInt(kernel.heapFixed.alloc(bytesToRead, false))) orelse kernel.panic("unable to allocate memory for fs detection");
+        var dmaBuffer = kernel.zeroes(DMABuffer);
+        dmaBuffer.virtualAddr = @intFromPtr(self.signatureBlock);
+        var request = kernel.zeroes(BlockDevice.AccessRequest);
+        request.device = self;
+        request.count = bytesToRead;
+        request.operation = read;
+        request.buffer = &dmaBuffer;
+        //TODO?: should have access
+
+        if (!self.checkMbr()) kernel.panic("Only MBR is supported\n");
+
+        kernel.heapFixed.free(@intFromPtr(self.signatureBlock), bytesToRead);
+    }
+    fn checkMbr(self: *@This()) bool {
+        if (MBR.getPartitions(self.signatureBlock, self.sectorCount)) {
+            for (driver.mbrPartitions) |partition| {
+                if (partition.present) {
+                    driver.partitionDevices.len += 1;
+                    //TODO!: partitions should be registered
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 };
